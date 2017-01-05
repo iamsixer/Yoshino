5c0e5514aabc495d1b763300f56d3ea81e18f495
Volio <volio.liu@gmail.com>
2017-01-05 22:31:27 +0800

    Remove form authorization

--- src/main/java/yoshino/config/SecurityConfig.java
+++ src/main/java/yoshino/config/SecurityConfig.java
@@ -1,16 +1,11 @@
 package yoshino.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import yoshino.services.UserService;
 
 /**
  * Created by Volio on 2016/12/18.
@@ -18,38 +13,6 @@ import yoshino.services.UserService;
 @Configuration
 @EnableOAuth2Sso
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    private UserService userService;
-
-    @Autowired
-    public SecurityConfig(UserService userService) {
-        this.userService = userService;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new StandardPasswordEncoder("53cr3t");
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService)
-                .passwordEncoder(passwordEncoder());
-    }
-
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .antMatchers("/account/**")
-//                .hasAnyAuthority("USER", "ADMIN")
-//                .antMatchers("/admin/**")
-//                .hasAnyAuthority("ADMIN")
-//                .anyRequest().permitAll().and()
-//                .formLogin().loginPage("/login").and()
-//                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-//                .logoutSuccessUrl("/login").and()
-//                .httpBasic();
-//    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
--- src/main/java/yoshino/services/UserService.java
+++ src/main/java/yoshino/services/UserService.java
@@ -1,46 +1,19 @@
 package yoshino.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import yoshino.models.User;
 import yoshino.repositories.UserRepository;
-
-import java.util.Collections;
 
 /**
  * Created by Volio on 2016/12/18.
  */
 @Service
-public class UserService implements UserDetailsService {
+public class UserService {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findOneByMail(email);
-        if (user == null)
-            throw new UsernameNotFoundException("user not exist");
-        return createSpringUser(user);
-    }
-
-    private org.springframework.security.core.userdetails.User createSpringUser(User user) {
-        return new org.springframework.security.core.userdetails.User(
-                user.getUsername(),
-                user.getPassword(),
-                Collections.singleton(createAuthority(user)));
-    }
-
-    private GrantedAuthority createAuthority(User user) {
-        return new SimpleGrantedAuthority(user.getRole());
     }
 }
